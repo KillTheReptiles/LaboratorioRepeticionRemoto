07b8940a4be894ddba8b9163a00c04a2f2019c26
Diaz Montanez <000401676@bga.upb>
2019-03-01 11:53:44 -0500

    tercer Algoritmo

---
+++ positivos.py
@@ -0,0 +1,14 @@
+print("Comienzo")
+print()
+numero = int(input("Escriba un numero positivo: "))
+while numero < 0:
+    print("Has escrito un numero negativo, Fin:D")
+    break
+
+while numero > 0:
+    print("Has escrito un numero positivo")
+    numero = int(input("Escriba positivo: "))
+
+
+print()
+print("Final")
