60136dace9f04c90289baa49ab088be6685ee8e4
Diaz Montanez <000401676@bga.upb>
2019-03-01 10:54:23 -0500

    Primer commit:D

---
+++ mayor.py
@@ -0,0 +1,24 @@
+print("Comienzo")
+print()
+mayor = int(input("Escriba un numero mayor a 0: "))
+
+while mayor < 0:
+    print("Has escrito un numero menor a 0")
+    mayor = int(input("Escriba un numero mayor a 0: "))
+while mayor > 0:
+    print("Has escrito un numero mayor a 0")
+    mayor = int(input("Escriba un numero mayor a 0: "))
+while mayor < 0:
+    print("Has escrito un numero menor a 0")
+    mayor = int(input("Escriba un numero mayor a 0: "))
+while mayor > 0:
+    print("Has escrito un numero mayor a 0")
+    mayor = int(input("Escriba un numero mayor a 0: "))
+
+
+
+
+print("Tu numero es mayor a cero, Gracias :D!")
+
+print()
+print("Final")
